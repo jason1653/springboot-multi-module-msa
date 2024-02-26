f6945132d7aeb42c8e017f4219b8b764e7f71929
jason <saga1654@kakao.com>
2024-02-26 16:11:55 +0900

    Aspect

--- common-logger/src/main/kotlin/io/jason/commonlogger/aspect/ElapseLoggingAspect.kt
+++ common-logger/src/main/kotlin/io/jason/commonlogger/aspect/ElapseLoggingAspect.kt
@@ -4,7 +4,6 @@ import io.jason.commonlogger.util.LoggerUtil
 import org.aspectj.lang.ProceedingJoinPoint
 import org.aspectj.lang.annotation.Around
 import org.aspectj.lang.annotation.Aspect
-import org.springframework.core.annotation.Order
 import org.springframework.stereotype.Component
 import org.springframework.util.StopWatch
 
