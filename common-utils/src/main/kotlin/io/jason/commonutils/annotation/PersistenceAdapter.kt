289ac93d4620da38d9fd68526f0127a027c1d07a
jason <saga1654@kakao.com>
2024-02-29 14:10:05 +0900

    헥사고날 어노테이션

---
+++ common-utils/src/main/kotlin/io/jason/commonutils/annotation/PersistenceAdapter.kt
@@ -0,0 +1,13 @@
+package io.jason.commonutils.annotation
+
+import org.springframework.core.annotation.AliasFor
+import org.springframework.stereotype.Component
+
+
+@Target(AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.RUNTIME)
+@Component
+annotation class PersistenceAdapter(
+    @get:AliasFor(annotation = Component::class)
+    val value: String = "",
+)
---
+++ common-utils/src/main/kotlin/io/jason/commonutils/annotation/UseCase.kt
@@ -0,0 +1,14 @@
+package io.jason.commonutils.annotation
+
+import org.springframework.core.annotation.AliasFor
+import org.springframework.stereotype.Component
+
+
+@Target(AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.RUNTIME)
+@Component
+annotation class UseCase (
+    @get:AliasFor(annotation = Component::class)
+    val value: String = "",
+
+)
