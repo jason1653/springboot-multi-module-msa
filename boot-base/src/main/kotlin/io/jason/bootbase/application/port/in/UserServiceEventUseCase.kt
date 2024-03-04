90dc08eeba7d16661344b06ac309e8f5b321ead4
jason <saga1654@kakao.com>
2024-03-04 18:04:28 +0900

    구조 변경

---
+++ boot-base/src/main/kotlin/io/jason/bootbase/application/port/in/UserServiceEventUseCase.kt
@@ -0,0 +1,4 @@
+package io.jason.bootbase.application.port.`in`
+
+interface UserServiceEventUseCase {
+}
---
+++ boot-base/src/main/kotlin/io/jason/bootbase/application/port/out/UserServiceEventPort.kt
@@ -0,0 +1,4 @@
+package io.jason.bootbase.application.port.out
+
+interface UserServiceEventPort {
+}
